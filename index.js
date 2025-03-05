d91fb3c2c21b2666ebfd45f7e222cf404cdc66a6
Yash Raj Singh <workmail.yrs@gmail.com>
2025-03-05 23:46:24 +0530

    added mongo DB and schemas

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
+const {UserModel, TodoModel} = require("./db");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://yash:hKimiPvfGZjxkpU9@cluster0.yvabp.mongodb.net/")
+
+JWT_SECRET_KEY = "YashCrazy"
 app.use(express.json());
-app.post('/signup', (req, res)=>{
-
+app.post('/signup', async (req, res)=>{
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+    await UserModel.insert({
+        email : email,
+        password : password,
+        name : name
+    })
+    res.json({
+        message : "You are logged in"
+    })
 });
-app.post('/login', (req, res)=>{
-
+app.post('/login', async (req, res)=>{
+    const email = req.body.email;
+    const password = req.body.password;
+    const user  = await UserModel.findOne({
+        email : email,
+        password : password
+    })
+    console.log(user);
+    if (user){
+        const token = jwt.sign({
+            id: user._id
+        },JWT_SECRET_KEY)
+        
+        res.json({
+             token : token
+        })
+    } else{
+        res.status(403).json({
+            message : "Incoreect Credentials"
+        })
+    }
 });
 app.post('/todo', (req, res)=>{
 
@@ -14,4 +49,4 @@ app.get('/todos', (req, res)=>{
 
 });
 
-app.listen(3000, console.log("App is running on the port 3000"));
+app.listen(3001, console.log("App is running on the port 3000"));
